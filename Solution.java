4031f7e3d7b27313be41198f85968d34fc2558b1
Wu <brian.wu@fiserv.com>
2023-10-31 14:16:34 -0400

    AH5

--- Solution.java
+++ Solution.java
@@ -1,12 +1,38 @@
+import java.util.HashMap;
+
 class Solution {
-    public static char mostFrequentChar(String s) {
-      
-      return;
-    }
-  
-    public static void main(String[] args) {
-      // Try out your test cases:
-      System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+  public static char mostFrequentChar(String s) {
+      int largest_count =0;
+      char most_freq = s.charAt(0);
+      //Hash map for frequency and index
+      HashMap<Character, Integer> h_map = new HashMap<Character, Integer>();
+      HashMap<Character, Integer> char_first_index = new HashMap<Character, Integer>();
+      for(int i = 0; i < s.length();i++){
+        //check if hashmap has key
+        if(h_map.containsKey(s.charAt(i))){
+          //update entry
+          h_map.put(s.charAt(i), h_map.get(s.charAt(i))+1);
+        }
+        else{ 
+          //add new entry
+          h_map.put(s.charAt(i), 0);
+          char_first_index.put(s.charAt(i), i);
+        }
+        //check if frequency is higher or if tie
+        if(h_map.get(s.charAt(i))>largest_count||(h_map.get(s.charAt(i))==largest_count && char_first_index.get(s.charAt(i))<char_first_index.get(most_freq))){
+          most_freq = s.charAt(i);
+          largest_count = h_map.get(s.charAt(i));
+        }
+      }
+      return most_freq;
     }
 
+  public static void main(String[] args) {
+    // Try out your test cases:
+    System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+    System.out.println(Solution.mostFrequentChar("david")); // -> 'd'
+    System.out.println(Solution.mostFrequentChar("mississippi")); // -> 'i'
+    System.out.println(Solution.mostFrequentChar("eleventennine")); // -> 'e'
   }
+
+}
